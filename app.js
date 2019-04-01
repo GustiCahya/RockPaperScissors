ec408c8033d938b5b7f55dae7dc2a7d712897708
Gusti Cahya <gusticahyax@yahoo.com>
2019-04-01 20:30:22 +0800

    javascript

---
+++ app.js
@@ -0,0 +1,37 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice(){
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    console.log("computerChoice => " + computerChoice);
+    console.log("userChoice => " + userChoice);
+}
+
+function main(){
+    rock_div.addEventListener('click', function(){
+        game('r');
+    });
+
+    paper_div.addEventListener('click', function(){
+        game('p');
+    });
+
+    scissors_div.addEventListener('click', function(){
+        game('s');
+    });
+}
+
+main();
